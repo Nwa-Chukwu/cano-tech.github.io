0fcec0d6ea21ce3b20c51a8523c0398cbef78f89
Nwa-Chukwu <augustinechikei@gmail.com>
2024-03-02 15:55:02 +0100

    my first commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+const bodyElem = document.querySelector("body");
+const toggleElem = document.querySelector(".fa-sun");
+
+toggleElem.addEventListener("click", ()=>{
+  bodyElem.classList.toggle("dark");
+
+  if (bodyElem.classList.contains("dark")) {
+    toggleElem.classList.replace("fa-sun", "fa-moon")
+  } else {
+    toggleElem.classList.replace("fa-moon", "fa-sun")
+  }
+});
